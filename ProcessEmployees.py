00cbbcf06bb42d8fd2b4de957d78ccedccf28eca
William <william_burns1@baylor.edu>
2023-02-13 13:49:12 -0600

    submitting quiz

--- ProcessEmployees.py
+++ ProcessEmployees.py
@@ -10,32 +10,28 @@ Iternate through the dictionary to print out their name and thier new salary (as
 
 import csv
 
-#open the file
+# open the file
+data = open('employee_data.csv', 'r')
 
 
+# create an empty dictionary
+dict = {}
 
+# use a loop to iterate through the csv file
+# check if the employee fits the search criteria
 
-#create an empty dictionary
+with open('employee_data.csv', 'rt') as data1:
+    data2 = csv.DictReader(data1, delimiter=',', quotechar='"')
+    for employee in data2:
+        employee["Salary"] = float(
+            employee["Salary"]) * 1.1 * float((employee["Title"] == "CSR"))
+    for employee in data2:
+        print("Manager Name:" + employee["First Name"] +
+              employee["Last Name"] + "Current Salary:" + employee["Salary"])
 
-
-#use a loop to iterate through the csv file
-
-
-    #check if the employee fits the search criteria
-
-
-    
 
 print()
 print('=========================================')
 print()
 
-#iternate through the dictionary and print out the key and value as per printout
-
-
-
-          
-        
-
-        
-    
+# iternate through the dictionary and print out the key and value as per printout
